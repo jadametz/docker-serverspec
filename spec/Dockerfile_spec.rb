512815d70a0ed3684b358a39ffa35d512deb960b
Jesse Adametz <jesseadametz@gmail.com>
2017-09-04 11:54:09 -0700

    Test image characteristics

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -3,13 +3,17 @@ require "docker"
 
 describe "Dockerfile" do
   before(:all) do
-    image = Docker::Image.build_from_dir('.')
-    image.tag(repo: 'jadametz/serverspec', tag: 'latest')
+    @image = Docker::Image.build_from_dir('.')
+    @image.tag(repo: 'jadametz/serverspec', tag: 'latest')
 
     set :os, family: :alpine
     set :backend, :docker
-    set :docker_image, image.id
+    set :docker_image, @image.id
     set :docker_container_create_options, { 'Entrypoint' => ['ash'] }
+  end
+
+  it "should have the maintainer label" do
+    expect(@image.json["Config"]["Labels"].has_key?("maintainer"))
   end
 
   describe package('build-base') do
@@ -34,5 +38,13 @@ describe "Dockerfile" do
     describe package(gem) do
       it { should be_installed.by('gem') }
     end
+  end
+
+  it "should execute rspec" do
+    expect(@image.json["Config"]["Entrypoint"][0]).to eq("rspec")
+  end
+
+  it "should pass the -h option by default" do
+    expect(@image.json["Config"]["Cmd"][0]).to eq("-h")
   end
 end
